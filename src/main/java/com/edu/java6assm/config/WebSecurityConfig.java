9553f4c9c1190e02dd5ac22e49776af3e0419b39
AnonymousDude <balisongian@gmail.com>
2022-08-21 14:20:36 +0700

    fix heroku 500 lan 4

--- src/main/java/com/edu/java6assm/config/WebSecurityConfig.java
+++ src/main/java/com/edu/java6assm/config/WebSecurityConfig.java
@@ -60,9 +60,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         protected void configure(HttpSecurity http) throws Exception {
                 http.csrf().disable().cors();
                 http.authorizeRequests()
-                                .antMatchers(HttpMethod.OPTIONS, "/").permitAll()
+                                .antMatchers(HttpMethod.POST, "/").permitAll()
                                 .antMatchers(
                                                 HttpMethod.GET,
+                                                "/",
                                                 "/home/**",
                                                 "/security/**",
                                                 "/rest/products/**",
